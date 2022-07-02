b7d3b3ddf6f8057b02fece161066b43f5a06bb5a
siddiqueath <MY_NAME@example.com>
2022-07-02 17:44:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+export default function App() {
+  let users = [
+    {
+      name: 'robert',
+      pic: 'https://images.pexels.com/photos/5506894/pexels-photo-5506894.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name: 'daniel',
+      pic: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600'   
+    },
+    {
+      name: 'chris',
+      pic: 'https://images.pexels.com/photos/356147/pexels-photo-356147.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map( user => <UserGrid name={user.name} pic={user.pic}/> )}
+    </div>
+   );
+}
+function Counter(){
+  let [like, setLike] = useState(0);
+  let [dislike, setDisLike] = useState(0);
+  return(
+    <div>
+      <button className='btn-like' onClick={()=>setLike(like+1)}>
+        👍{like}
+      </button>
+      <button className='btn-dislike' onClick={()=>setDisLike(dislike+1)}>
+        👎{dislike}
+      </button>
     </div>
   );
 }
-
-export default App;
+function UserGrid({name, pic}){
+  return(
+    <div className='user-grid'>
+      <img class='prof-pic' src={pic} alt={name}/>
+      <h1>Hello! {name}</h1>
+      <Counter/>
+    </div>
+  );
+}
